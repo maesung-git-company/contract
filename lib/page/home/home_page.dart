177d7087573ec6a2eb8d4d9a75758a6909635af3
jeery567 <jeery567@gmail.com>
2024-06-02 14:13:01 +0000

    re:home_page.dart

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -18,39 +18,16 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  Widget build(BuildContext context) { // todo A5 로그아웃 버튼 만들어주셈 ## Global.prefs.remove("user_id"); ## 호출하고 메인화면으로 돌아가든 하면 될듯
+  Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TitleMain',
-        home: Scaffold(
-            body: Stack(
-                children: [
-                  Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          StepCounter() // todo A6 소나무 png 따서 초록색 차는거 그거 구현좀 ㅎ
-                        ],
-                      )
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Stack(
-                        children: [
-                          OutlinedButton( // todo A2 학사모 모양 버튼인데 이거 클래스로 분리할 수 있으면 해주고 크기 키우거나 해서 잘보이게 해주셈
-                            onPressed: sendClassListPageNav,
-                            style: ButtonStyle(),
-                            child: Icon(
-                              Icons.school_sharp,
-                              color: Colors.black,
-                            ),
-                          ),
-                          SizedBox( width: 0, height: 200, ),
-                        ]
-                    ),
-                  ),
-                ]
-            )
-        )
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: Container(
+          child: Text("hello"),
+        ),
+      ),
     );
   }
 }
