c1d06f8b175858857f56a20417b5e73dd0fc4d96
jeery567 <jeery567@gmail.com>
2024-06-05 05:35:18 +0000

    re:Home_Page.dart

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:contract/page/class_list/class_list_page.dart';
-import 'package:contract/widget/step_counter.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,12 +21,85 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Home Page'),
+          leading: IconButton(
+            onPressed: () {
+              sendClassListPageNav();
+            }, icon: Icon(Icons.menu)),
+          backgroundColor: Colors.white,
+          title: const Text('Step Counter'),
         ),
         body: Container(
-          child: Text("hello"),
+          child: Column(
+            children: [
+              Flexible(
+                flex: 3, 
+                child: Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: StepIndicator(),
+                ),
+              )),
+              Flexible(
+                flex: 7, 
+                child: Container(
+                  color: Colors.green,
+                  child: const Center(
+                    child: ClassIndicator(),
+                ),
+              )),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class StepIndicator extends StatefulWidget {
+  const StepIndicator({super.key});
+
+  @override
+  State<StepIndicator> createState() => _StepIndicatorState();
+}
+
+class _StepIndicatorState extends State<StepIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 20, 20, 30),
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Text('Current Steps',
+                 style: TextStyle(fontSize: 16,)),
+                Text('0',
+                 style: TextStyle(fontSize: 32)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*--------------------------------------------------------------------*/
+
+class ClassIndicator extends StatefulWidget {
+  const ClassIndicator({super.key});
+
+  @override
+  State<ClassIndicator> createState() => _ClassIndicatorState();
+}
+
+class _ClassIndicatorState extends State<ClassIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Class Indicator'),
     );
   }
 }
